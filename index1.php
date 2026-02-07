a5f618a61280f5a05e1b71eead43320e224b6637
Rishab Raj <rr0948555@gmail.com>
2026-02-07 21:59:18 +0530

    PHP

---
+++ index1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+   $name = "Alex Pandian";
+  ?>
+
+  <p>Hi! My name is <?php echo $name; ?>, and I'm learning PHP!</p>
+  
+</body>
+</html>
