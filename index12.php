a9a7da323ab721a057af4a835198d3792aeb499a
Rishab Raj <rr0948555@gmail.com>
2026-02-17 22:58:21 +0530

    Array Methods

---
+++ index12.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+   <?php 
+   
+      // $string = "Hello World!";
+
+      // echo strlen($string);
+      // echo strpos($string, "o");  // 4
+
+      // echo str_replace("World!", "Alex", $string); // Hello Alex
+
+      // echo strtolower($string); // hello world
+
+      // echo substr($string, 2, 2);  // ll
+
+      // print_r(explode(" ", $string));
+
+      $number = -5.5;
+
+      echo abs($number);
+
+   ?>
+</body>
+</html>
