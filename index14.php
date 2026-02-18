89632268d7c88fcaea302e3045ff652978c86d5c
Rishab Raj <rr0948555@gmail.com>
2026-02-18 21:55:27 +0530

    Push Pop in Array

---
+++ index14.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+  <?php
+   
+    // $number = -5.5;
+
+    // echo sqrt(2);
+    // echo rand(1, 100);
+
+    $array = ["apple", "banana", "orange"];
+
+    // echo count($array);
+    // echo is_array($array);
+    array_push($array, "kiwi");
+    print_r($array);
+
+    array_pop($array);
+    print_r($array);
+
+
+  ?>
+  
+</body>
+</html>
