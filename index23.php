140f1328534df511f00480ac0f94999f3769358c
Rishab Raj <rr0948555@gmail.com>
2026-02-20 22:00:08 +0530

    Calculator

---
+++ index23.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+  
+    // When strict type is  enabled then there will be no error in defining string and passing number
+
+     function calculator(int $num01, int $num02)
+     {
+         $result = $num01 + $num02;
+         return $result;
+     }
+
+     $test = calculator(2, 5);
+     echo $test;
+  ?>
+  
+</body>
+</html>
