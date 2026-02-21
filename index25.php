c628f13b0485d4cbcffe06123e9498bf127596a4
Rishab Raj <rr0948555@gmail.com>
2026-02-21 14:51:45 +0530

    Local Variable accessing Global Variable

---
+++ index25.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Scopes</title>
+</head>
+<body>
+     <?php
+
+      $test = "Alex";
+     
+      function myFunction($test)
+      {
+           // Defines a local variable
+           $localVar = "Hello, world!";
+
+           // Use the local variable
+           return $test; // Output: Hello, world!
+      }
+
+      echo myFunction($test);
+       
+     ?>
+
+</body>
+</html>
