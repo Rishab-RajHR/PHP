4ec2e4057084c5ef65ea59284b8881030257cb75
Rishab Raj <rr0948555@gmail.com>
2026-02-21 15:01:12 +0530

    Static

---
+++ index27.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Scopes</title>
+</head>
+<body>
+     <?php
+
+      $test = "Alex";
+     
+      function myFunction()
+      {
+          // Declare a static variable
+          $staticVar = 0;
+
+          // Increment the static variable
+          $staticVar++;
+
+          // Use the static variable
+          return $staticVar;
+      }
+
+      echo myFunction();
+      echo myFunction();
+       
+     ?>
+
+</body>
+</html>
