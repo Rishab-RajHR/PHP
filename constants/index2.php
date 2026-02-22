8a890da386d5e6be46530ee1eb1d24f19154c623
Rishab Raj <rr0948555@gmail.com>
2026-02-22 21:35:39 +0530

    Constants

---
+++ constants/index2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+    
+    define("PI", 3.14);
+    define("NAME", "Daniel");
+    define("IS_ADMIN", true);
+    echo IS_ADMIN;
+    
+  ?>
+</body>
+</html>
