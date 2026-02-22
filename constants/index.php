6d8cfed9a3c0ee7611387ff41f306b4f5621c81e
Rishab Raj <rr0948555@gmail.com>
2026-02-22 21:33:14 +0530

    Constants{

---
+++ constants/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+    
+     define("PI", 3.14);
+    //  define("PI", 4.14); // This will give an error 
+     echo PI;
+    
+  ?>
+</body>
+</html>
