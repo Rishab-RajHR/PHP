a9cf46da5a8e4b9a52ba96ba36bc3ef9f818a705
Rishab Raj <rr0948555@gmail.com>
2026-02-22 21:38:01 +0530

    Constants Function

---
+++ constants/index3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php 
+    
+    define("PI", 3.14);
+    define("NAME", "Daniel");
+    define("IS_ADMIN", true);
+    // echo IS_ADMIN;
+
+    function test()
+    {
+        echo PI;
+    }
+
+    test();
+    
+  ?>
+</body>
+</html>
