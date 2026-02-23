506c10bf8ac6b6364ba2d647e17310a3211a828a
Rishab Raj <rr0948555@gmail.com>
2026-02-23 15:09:24 +0530

    Foreach Loop

---
+++ constants/index9.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<?php 
+  
+    $fruits = array("Apple", "Banana", "Orange");
+
+    foreach ($fruits as $fruit) {
+         echo "This is a " . $fruit . "<br>";
+    }
+?>
+  
+</body>
+</html>
