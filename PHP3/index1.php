30a5775a126c506482e1008075c9656679e6d36a
Rishab Raj <rr0948555@gmail.com>
2026-03-27 22:01:43 +0530

    COmments

---
+++ PHP3/index1.php
@@ -0,0 +1,11 @@
+<?php 
+   $a = 5;
+   $b = 10;
+   $c = $a+$b;
+
+   echo "First value = $a . <br>";
+   echo "Second value = $a . <br>";
+  //  echo keyword is not case sensitive
+   echo "Addition = $c";
+
+?>
